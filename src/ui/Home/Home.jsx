2dc7cbb9124d7160cd43632aeee5ac0133f1fac4
PouyaBirvand <pooyabirvand@gmail.com>
2025-07-10 20:37:12 +0330

    chore: edit layout homepage

--- src/ui/Home/Home.jsx
+++ src/ui/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   const username = useSelector((state) => state.user.username);
 
   return (
-    <div className='flex min-h-screen items-center justify-center px-4'>
+    <div className='mx-auto max-w-6xl px-4 py-12'>
       <div className='mx-auto max-w-4xl text-center'>
         <HeroSection />
         <CTASection username={username} />
