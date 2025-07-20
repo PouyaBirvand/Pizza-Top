690aeb990ad597de3f22e34c5a33337f7357c574
PouyaBirvand <pooyabirvand@gmail.com>
2025-07-20 05:21:26 +0330

    chore: scroll to 0 when you navigate to routes

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,18 @@
 import Header from './Header/Header';
 import Loader from './Loader';
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 import CartOverview from '../features/cart/components/CartOverview';
 import Footer from './Footer/Footer';
+import { useEffect } from 'react';
 
 function AppLayout() {
+  const { pathname } = useLocation();
   const navigation = useNavigation();
   const isLoading = navigation.state === 'loading';
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <div className='min-h-screen bg-gradient-to-br from-emerald-50 via-white to-slate-100 transition-all duration-300 dark:from-slate-900 dark:via-slate-800 dark:to-emerald-950'>
